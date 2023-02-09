67e14b569a8bd86d7577c0010e7f34fd28cf01d6
Jake Nicholl <2712041N@student.gla.ac.uk>
2023-02-09 22:11:34 +0000

    Chapter 9.5

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
@@ -11,3 +11,4 @@ class PageAdmin(admin.ModelAdmin):
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
